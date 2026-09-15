5a2e559695a959a9de8d4e0eda7537a5d6cd98f3
cristlf <crislfiebes@gmail.com>
2026-09-15 16:58:08 -0300

    atulizado

---
+++ ProdutoApi/Services/produtoServices.cs
@@ -0,0 +1,35 @@
+﻿using ProdutoApi.dto;
+using ProdutoApi.interfaces;
+using System.Security.Cryptography.X509Certificates;
+namespace ProdutoApi.Services
+{
+    public class ProdutoServices : IProdutoService
+
+    {
+        private readonly IProdutoRepository _repository;
+        public ProdutoServices(IProdutoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ProdutoResponseDTO> CreateProdutoAsync(ProdutoRequestDTO produtoRequestDTO)
+
+        {  var produto = new Produto()
+        {
+            Nome = produtoRequestDTO.Nome,
+            Preco = produtoRequestDTO.Preco,
+            Quantidade = produtoRequestDTO.Quantidade,
+            Ativo = produtoRequestDTO.Ativo ?? true
+        };
+
+               public async Task<bool> DeleteProdutoAsync(int produtoId)
+        { 
+            if (produtoId <= 0)
+            {
+                throw new ArgumentException("O ID do produto deve ser maior que zero.", nameof(produtoId));
+            } return await _repository.DeletAsync(produtoId);
+
+
+            {
+            
+}
---
+++ ProdutoApi/interface/IProdutoService.cs
@@ -0,0 +1,12 @@
+﻿using ProdutoApi.dto;
+namespace ProdutoApi. interfaces
+{
+    public interface IProdutoService
+    {
+    Task<IEnumerable<ProdutoResponseDTO>> GetAllAsync();
+        Task<ProdutoResponseDTO?> GetByIdAsync(int produtosId);
+        Task<ProdutoResponseDTO> CreateAsync(ProdutoRequestDTO produtoRequestDTO);
+        Task<ProdutoResponseDTO> UpdateAsync(int produtosId, ProdutoRequestDTO produtoRequestDTO);
+        Task<bool> DeleteAsync(int produtosId);
+    }
+}
